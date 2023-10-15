5c4ef31f96314a794d8af4ee6ca2bbd461235459
Lex <Lexor.V.A.S@yandex.ru>
2023-10-16 00:28:28 +0300

    add fount

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,9 +40,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
+  @include style1;
 }
 ul {
   list-style-type: none;
